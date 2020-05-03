cc6a267fa83357d469738f0174ebccb67ffc2f73
TiffanyCowman <61311153+TiffanyCowman@users.noreply.github.com>
2020-05-03 07:25:49 +0100

    Delete Form .py

--- service1/application/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from application import db
-from wtforms import submitField 
-from application.models import result 
-
-class generateForm(FlaskForm):
-    submit = submitFeild('Generate')
