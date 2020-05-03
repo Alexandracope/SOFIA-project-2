a06c45e605be4418210147d0939187010ff02bd1
Yasir Satti <yassir_satti@hotmail.com>
2020-05-04 00:34:03 +0100

    Tests for service1, 2, 3 & part of 4 + db table creation

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
-from application import app
+from application import app, db
+from application.models import result
 import requests
 
 @app.route('/')
@@ -9,6 +10,9 @@ def home():
     response = requests.get('http://service4:5003/generator')
     random = response.text
     
+    # db.session.add(random)
+    # db.session.commit() 
+
     # print (random)
     
     # 6209a358e24e918980493bb7749ae3f2be5545a9
