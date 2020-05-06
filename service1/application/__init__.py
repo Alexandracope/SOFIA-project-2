b9be4b8309235196baa4fd2cd6c5b4d45e00125e
TiffanyCowman <tiffany.cowman@outlook.com>
2020-05-06 17:53:10 +0100

    env erased

--- service1/application/__init__.py
+++ service1/application/__init__.py
@@ -9,9 +9,9 @@ from os import getenv
 app = Flask(__name__)
 mysql = MySQL(app)
 
-app.config['MYSQL_HOST'] = getenv('MYSQL_HOST')
-app.config['MYSQL_USER'] = getenv('MYSQL_USER')
-app.config['MYSQL_DB'] = getenv('MYSQL_DB')
-app.config['MYSQL_PASSWORD'] = getenv('MYSQL_PASSWORD')
+app.config['MYSQL_HOST'] = ''
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_DB'] = ''
+app.config['MYSQL_PASSWORD'] = ''
 
 from application import routes
