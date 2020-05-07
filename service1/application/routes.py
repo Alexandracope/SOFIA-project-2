a48f37a0c6e930e891338a0b3e74a1c3d10e5527
Yasir Satti <yassir_satti@hotmail.com>
2020-05-07 08:19:13 +0100

    service4 tests changes

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,6 +1,5 @@
 from flask import render_template
 from application import app
-from flask_mysqldb import MySQL
 import requests
 
 @app.route('/')
@@ -10,10 +9,4 @@ def home():
     response = requests.get('http://service4:5003/generator')
     random = response.text
     
-    # Mysql commands used to insert result of get request to database table
-    # cur = mysql.connection.cursor()
-    # cur.execute("INSERT INTO result(result)VALUES(%s)", [random])
-    # mysql.connection.commit()
-    # cur.close()
-
     return render_template('home.html',random=random, title='Home')
