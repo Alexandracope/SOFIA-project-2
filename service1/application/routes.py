728aa3a3406493c91e714837c2d2bc4bbc1cd5b9
Yasir Satti <yassir_satti@hotmail.com>
2020-05-07 08:27:49 +0100

    service4 more tests

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from application import app
+# from flask_mysqldb import MySQL
 import requests
 
 @app.route('/')
@@ -9,4 +10,13 @@ def home():
     response = requests.get('http://service4:5003/generator')
     random = response.text
     
+    # Mysql commands used to insert result of get request to database table
+    # cur = mysql.connection.cursor()
+    #table is created in database below code
+    # cur.execute('''CREATE TABLE result (id INTEGER, result VARCHAR(50))''')
+    # hash code above after first run should come up with error table already created when button pressed then unhash the last three lines
+    # cur.execute("INSERT INTO result(result)VALUES(%s)", [random])
+    # mysql.connection.commit()
+    # cur.close()
+
     return render_template('home.html',random=random, title='Home')
